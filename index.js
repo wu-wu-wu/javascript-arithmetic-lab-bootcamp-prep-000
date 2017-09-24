ea66db9e66ad1a31a7f4b071929140130f048885
Suzanne Wu <wu.wu.wu.world@gmail.com>
2017-09-24 02:11:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,9 +23,10 @@ function dec(n) {
 }
 
 function makeInt(n) {
-  return parseInt(n)
-}
-
-function makeInt(n) {
   return parseInt(n, 10)
 }
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+
